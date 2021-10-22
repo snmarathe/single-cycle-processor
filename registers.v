ee67c70946e09670ad977e5ec120a72c5ad36c0c
snmarathe <67182099+snmarathe@users.noreply.github.com>
2021-10-22 15:53:37 +0530

    async reads

--- registers.v
+++ registers.v
@@ -11,14 +11,17 @@ module registers (
     input [4:0] write_reg,
     input reg_write_flag,
     input [31:0] data,
-    output reg [31:0] read_data1,
-    output reg [31:0] read_data2
+    output [31:0] read_data1,
+    output [31:0] read_data2
 );
 
 //32 32-bit registers
 reg [31:0] regs [31:0];
 integer c;
 
+assign read_data1 = regs[read_reg1];
+assign read_data2 = regs[read_reg2];
+    
 always @ (posedge clk or posedge rst)
 begin
     if (rst)
@@ -34,8 +37,7 @@ begin
         regs[write_reg] <= data;
     end
 
-    read_data1 <= regs[read_reg1];
-    read_data2 <= regs[read_reg2];
+    else regs[write_reg] <= regs[write_reg];
 end
 
 endmodule
