8c522c3d1c248822c5119e276a5460a9ae5c170d
snmarathe <snmarathe2000@gmail.com>
2021-10-22 12:04:08 +0530

    updated with mips encoding, add write_reg mux

--- flags.v
+++ flags.v
@@ -1,4 +1,15 @@
-//All flags (in blue in the diagram) are generated based on the fields in the 32 bit instruction
+/*
+All flags (in blue in the diagram) are generated based on the opcode field in the 32 bit instruction
+R format    000000
+        add 100000
+        sub 100010
+        and 100100 
+        or  100101
+lw          100011
+sw          101011
+beq         000100
+j           000010
+*/
 
 module flags (
     input [31:0] instruction,
@@ -12,6 +23,17 @@ module flags (
     output jump_select
 );
 
+wire [5:0] opcode;
+assign opcode = instruction[31:26];
 
+assign reg_write_flag = (opcode == 6'b000000 || opcode == 6'b100011);
+assign data_write_flag = (opcode == 6'b101011);
+assign data_read_flag = (opcode == 6'b100011);
+assign ALU_operand_select = (opcode == 6'101011 || opcode == 6'b100011);
+assign send_to_reg_select = (opcode == 6'b100011);
+assign branch_select = (opcode == 6'b000100);
+assign jump_select = (opcode == 6'b000010);
+assign ALU_op[1] = (opcode == 6'b000000);
+assign ALU_op[0] = (opcode == 6'b000100);
 
 endmodule
