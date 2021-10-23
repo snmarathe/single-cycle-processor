553789be1c2e5f01e874694be8e235b5f1644072
snmarathe <snmarathe2000@gmail.com>
2021-10-23 19:56:09 +0530

    added write_reg mux

--- flags.v
+++ flags.v
@@ -15,6 +15,7 @@ R format        10
 module flags (
     input [31:0] instruction,
     output [1:0] ALU_op,
+    output write_reg_mux_select,
     output reg_write_flag,
     output data_write_flag,
     output data_read_flag,
@@ -27,6 +28,7 @@ module flags (
 wire [5:0] opcode;
 assign opcode = instruction[31:26];     //6 bit opcode field
 
+assign write_reg_mux_select = (opcode == 6'b000000);        //R format
 assign reg_write_flag = (opcode == 6'b000000 || opcode == 6'b100011);   //R format OR lw
 assign data_write_flag = (opcode == 6'b101011);     //sw
 assign data_read_flag = (opcode == 6'b100011);      //lw
